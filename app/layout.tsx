afacc8e45678e5f45f8de9971249f373ba87c8af
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-10-01 08:37:25 -0400

    latest commit

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "StockLens",
+  description:
+    "Track real-time stock prices, get personalized alerts and explore detailed company insights.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" type="image/x-icon" href="/logo.png" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <Button className="cursor-pointer bg-slate-100 text-slate-900">
+          Click
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
