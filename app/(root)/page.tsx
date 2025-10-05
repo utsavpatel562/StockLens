d290b575d39cff5d19494b2b46307f1464804768
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-10-05 13:15:08 -0400

    latest commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,20 @@
+import TradingViewWidget from "@/components/TradingViewWidget";
+import { MARKET_OVERVIEW_WIDGET_CONFIG } from "@/lib/constants";
+
 const Home = () => {
   return (
     <>
-      <div className="flex h-screen home-wrapper">Home</div>
+      <div className="flex min-h-screen home-wrapper">
+        <section className="grid w-full gap-8 home-section">
+          <div className="md:col-span-1 xl:col-span-1">
+            <TradingViewWidget
+              title="Market Overview"
+              scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js"
+              config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            />
+          </div>
+        </section>
+      </div>
     </>
   );
 };
---
+++ components/TradingViewWidget.tsx
@@ -0,0 +1,50 @@
+"use client";
+import useTradingViewWidget from "@/hooks/useTradingViewWidget";
+import React, { useRef, memo, RefObject } from "react";
+
+interface TradingViewWidgetProps {
+  title?: string;
+  scriptUrl: string;
+  config: Record<string, unknown>;
+  height?: number;
+  className?: string;
+}
+
+const TradingViewWidget = ({
+  title,
+  scriptUrl,
+  config,
+  height = 600,
+  className,
+}: TradingViewWidgetProps) => {
+  const containerRef: RefObject<HTMLDivElement | null> = useTradingViewWidget(
+    scriptUrl,
+    config,
+    height
+  );
+
+  return (
+    <div
+      className="tradingview-widget-container"
+      ref={containerRef}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <div
+        className="tradingview-widget-container__widget"
+        style={{ height: "calc(100% - 32px)", width: "100%" }}
+      ></div>
+      <div className="tradingview-widget-copyright">
+        <a
+          href="https://www.tradingview.com/symbols/NASDAQ-AAPL/"
+          rel="noopener nofollow"
+          target="_blank"
+        >
+          <span className="blue-text">AAPL stock chart</span>
+        </a>
+        <span className="trademark"> by TradingView</span>
+      </div>
+    </div>
+  );
+};
+
+export default memo(TradingViewWidget);
