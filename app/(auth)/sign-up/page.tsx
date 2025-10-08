a5efe774a8783b270c06c2b8efaf7585fcd11743
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-10-08 09:46:16 -0400

    latest commit

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CountrySelectField } from "@/components/forms/CountrySelectField";
+import FooterLink from "@/components/forms/FooterLink";
 import InputField from "@/components/forms/InputField";
 import SelectField from "@/components/forms/SelectField";
 import { Button } from "@/components/ui/button";
@@ -116,6 +117,11 @@ const SignUp = () => {
         >
           {isSubmitting ? "Creating account" : "Start Your Investing Journey"}
         </Button>
+        <FooterLink
+          linkText="Sign in"
+          text="Already have an account?"
+          href="/sign-in"
+        />
       </form>
     </div>
   );
