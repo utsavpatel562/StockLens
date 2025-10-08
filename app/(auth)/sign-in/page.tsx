390f5613b0235fa1d6892968354366988ee9c479
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-10-08 09:56:51 -0400

    latest commit

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,81 @@
+"use client";
+import FooterLink from "@/components/forms/FooterLink";
+import InputField from "@/components/forms/InputField";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 const SignIn = () => {
-  return <div>SignInPage</div>;
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInFormData>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onBlur",
+  });
+
+  const onSubmit = async (data: SignInFormData) => {
+    try {
+      const result = await signInWithEmail(data);
+      if (result.success) router.push("/");
+    } catch (e) {
+      console.error(e);
+      toast.error("Sign in failed", {
+        description: e instanceof Error ? e.message : "Failed to sign in.",
+      });
+    }
+  };
+
+  return (
+    <>
+      <h1 className="form-title">Welcome back</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="example@mail.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: "Email is required",
+            pattern: /^\w+@\w+\.\w+$/,
+          }}
+        />
+
+        <InputField
+          name="password"
+          label="Password"
+          placeholder="Enter your password"
+          type="password"
+          register={register}
+          error={errors.password}
+          validation={{ required: "Password is required", minLength: 8 }}
+        />
+
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="indigo-btn w-full mt-5"
+        >
+          {isSubmitting ? "Signing In" : "Sign In"}
+        </Button>
+
+        <FooterLink
+          text="Don't have an account?"
+          linkText="Create an account"
+          href="/sign-up"
+        />
+      </form>
+    </>
+  );
 };
 
 export default SignIn;
