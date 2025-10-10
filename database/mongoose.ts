33a51acc7e57305c0ee359f192519dfe4b9beb33
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-10-10 18:29:43 -0400

    latest commit

--- database/mongoose.ts
+++ database/mongoose.ts
@@ -26,4 +26,6 @@ export const connectToDatabase = async () => {
         throw err;
     }
     console.log(`Connected to database ${process.env.NODE_ENV} - ${MONGODB_URI}`);
+
+    return cached.conn
 }
