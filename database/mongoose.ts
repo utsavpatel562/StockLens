090133a6e21540ca78fc25069b69a57d7b9e3d5d
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-10-10 18:15:05 -0400

    latest commit

--- database/mongoose.ts
+++ database/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URI = process.env.MONGODB_URL;
 
 declare global {
     var mongooseCache: {
@@ -14,10 +14,10 @@ if(!cached) {
     cached = global.mongooseCache = {conn:null, promise: null};
 }
 export const connectToDatabase = async () => {
-    if(!MONGODB_URL) throw new Error('MONGODB_URL must be set within .env');
+    if(!MONGODB_URI) throw new Error('MONGODB_URL must be set within .env');
     if(cached.conn) return cached.conn;
     if(!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URL, {bufferCommands : false})
+        cached.promise = mongoose.connect(MONGODB_URI, {bufferCommands : false})
     }
     try {
         cached.conn = await cached.promise;
@@ -25,5 +25,5 @@ export const connectToDatabase = async () => {
         cached.promise = null;
         throw err;
     }
-    console.log(`Connected to database ${process.env.NODE_ENV} ${MONGODB_URL}`);
+    console.log(`Connected to database ${process.env.NODE_ENV} - ${MONGODB_URI}`);
 }
