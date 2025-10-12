6f5f3957d96e00186eca330618a5797277086fec
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-10-12 18:07:18 -0400

    latest commit

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,7 +1,8 @@
 import { inngest } from "@/lib/inngest/client"
+import { sendSigUpEmail } from "@/lib/inngest/functions"
 import {serve} from "inngest/next"
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
-    functions: [],
+    functions: [sendSigUpEmail],
 })
--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -1,4 +1,5 @@
 import {inngest} from "@/lib/inngest/client"
+import { PERSONALIZED_WELCOME_EMAIL_PROMPT } from "./prompts"
 
 export const sendSigUpEmail = inngest.createFunction(
     {id: 'sign-up-email'},
@@ -10,5 +11,28 @@ export const sendSigUpEmail = inngest.createFunction(
         - Risk tolerance: ${event.data.riskTolerance}
         - Preferred industry: ${event.data.preferredIndustry} 
         `
+
+        const prompt = PERSONALIZED_WELCOME_EMAIL_PROMPT.replace('{{userProfile}}', userProfile)
+
+        const response = await step.ai.infer('generate-welcome-intro', {
+            model: step.ai.models.gemini({model: 'gemini-2.0-flash-lite'}), 
+                body: {
+                    contents: [
+                        {
+                            role: "user",
+                            parts: [
+                                { text: prompt }
+                            ]
+                        }]
+                }
+            })
+            await step.run('send-welcome-email', async() => {
+                const part = response.candidates?.[0]?.content?.parts?.[0];
+                const introText = (part && 'text' in part ? part.text : null) || "Thanks for joining StockLens. You now have the tools to track markets and make smarter moves."
+            })
+        return {
+            success: true,
+            message: 'Welcome email sent successfully'
+        }
     }
 )
