a65b4bf8e67645cd89d98a39efa1ddebc0a9055e
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-10-13 11:44:57 -0400

    latest commit

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import { WELCOME_EMAIL_TEMPLATE } from "./templates";
 
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -7,3 +8,15 @@ export const transporter = nodemailer.createTransport({
         pass: process.env.NODEMAILER_PASSWORD!,
     }
 })
+
+export const sendWelcomeEmail = async({email, name, intro}: WelcomeEmailData) => {
+    const htmlTemplate = WELCOME_EMAIL_TEMPLATE.replace('{{name}}', name).replace('{{intro}}', intro);
+    const mailOptions = {
+        from: `"StockLens" <stocklens@market.pro>`,
+        to: email,
+        subject: `Welcome to StockLens - your stock market toolkit is ready!`,
+        text: 'Thanks for joining StockLens',
+        html: htmlTemplate,
+    }
+    await transporter.sendMail(mailOptions);
+}
